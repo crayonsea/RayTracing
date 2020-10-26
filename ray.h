10103d116f122e2ed4b1e3dce77571521cd3f0f6
crayon <15651875728@163.com>
2020-10-27 00:04:00 +0800

    feat(*): raytrace in one weekend 初始化

---
+++ ray.h
@@ -0,0 +1,25 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class Ray {
+    public:
+        Point3 orig;
+        Vec3 dir;
+
+    public:
+        Ray() {}
+        Ray(const Point3& origin, const Vec3& direction)
+            : orig(origin), dir(direction)
+        {}
+
+        Point3 origin() const  { return orig; }
+        Vec3 direction() const { return dir; }
+
+        Point3 at(double t) const {
+            return orig + t * dir;
+        }
+};
+
+#endif
